975c8f1abcbf34c104d273a1d8e8072844f8fc7c
Akshat Garg <coolakshat24@gmail.com>
2017-02-19 15:31:22 +0530

    Add the scripts: downloader.py, fav.py, newsteller.py, page.py

---
+++ page.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+#Gets the index page of mangareader.net
+
+os.system("wget -q -O index.html www.mangareader.net")
+
+#Open the index page on Google Chrome or Firefox according to the users choice
+
+if "-f" in sys.argv :
+    os.system("firefox index.html")
+else :
+    os.system("google-chrome-stable index.html")
+
+
+
